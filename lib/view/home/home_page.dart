012736a36bcd79b67c7924e70ae77029cbaa2926
Desrielkiki <desriel7c6@gmail.com>
2024-10-25 21:43:12 +0700

    add search function

--- lib/view/home/home_page.dart
+++ lib/view/home/home_page.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:shapee_app/database/helper/helper.dart';
 import 'package:shapee_app/view/cart/cart_page.dart';
 import 'package:shapee_app/view/detail/detail_page.dart';
-import 'package:shapee_app/view/chat/list_chat_page.dart'; 
+import 'package:shapee_app/view/chat/list_chat_page.dart';
 import 'package:shapee_app/database/data/product_data.dart';
 
 class HomePage extends StatefulWidget {
@@ -13,12 +13,58 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  TextEditingController _searchController = TextEditingController();
+  List<Map<String, dynamic>> _filteredProducts = ProductData.popularProduct;
+
+  @override
+  void initState() {
+    super.initState();
+    _searchController.addListener(_onSearchChanged);
+  }
+
+  @override
+  void dispose() {
+    _searchController.removeListener(_onSearchChanged);
+    _searchController.dispose();
+    super.dispose();
+  }
+
+  void _onSearchChanged() {
+    setState(() {
+      _filteredProducts = ProductData.popularProduct
+          .where((product) => product['name']!
+              .toLowerCase()
+              .contains(_searchController.text.toLowerCase()))
+          .toList();
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Shapee App',
-            style: TextStyle(fontWeight: FontWeight.bold, color: Color.fromARGB(255, 0, 17, 114),)),
+        title: Container(
+          height: 40,
+          decoration: BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.circular(8),
+          ),
+          child: TextField(
+            controller: _searchController,
+            decoration: const InputDecoration(
+              prefixIcon: Icon(
+                Icons.search,
+                color: Colors.grey,
+                size: 24,
+              ),
+              hintText: 'Cari produk...',
+              border: InputBorder.none,
+              hintStyle: TextStyle(color: Colors.grey),
+              contentPadding: EdgeInsets.only(left: -10, top: 10, bottom: 9),
+            ),
+            style: const TextStyle(color: Colors.black),
+          ),
+        ),
         backgroundColor: const Color.fromARGB(255, 114, 236, 255),
         actions: [
           IconButton(
@@ -33,7 +79,6 @@ class _HomePageState extends State<HomePage> {
           IconButton(
             icon: const Icon(Icons.message),
             onPressed: () {
-              
               Navigator.push(
                 context,
                 MaterialPageRoute(builder: (context) => const ListChatPage()),
@@ -45,106 +90,119 @@ class _HomePageState extends State<HomePage> {
       body: SingleChildScrollView(
         child: Column(
           children: [
-            const Padding(
-              padding: EdgeInsets.all(10.0),
-              child: Text(
-                "Produk Populer",
-                style: TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold),
-              ),
-            ),
-            GridView.builder(
-              physics: const NeverScrollableScrollPhysics(),
-              shrinkWrap: true,
-              gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                crossAxisCount: 2,
-                crossAxisSpacing: 0,
-                mainAxisSpacing: 0,
-                childAspectRatio: 0.75,
-              ),
-              itemCount: ProductData.popularProduct.length,
-              itemBuilder: (context, index) {
-                final product = ProductData.popularProduct[index];
-                return GestureDetector(
-                  onTap: () {
-                    
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                        builder: (context) => DetailPage(
-                          images: List<String>.from(product['images']),
-                          name: product['name']!,
-                          price: product['price']!,
-                          sold: int.tryParse(product['sold'].toString()) ??
-                              0, 
-                          description: product['description'] ?? '',
+            _filteredProducts.isEmpty
+                ? const Center(
+                    child: Padding(
+                      padding: EdgeInsets.symmetric(vertical: 100.0),
+                      child: Text(
+                        "Produk tidak ditemukan",
+                        style: TextStyle(
+                          fontSize: 24.0, // Ukuran font lebih besar
+                          fontWeight: FontWeight.bold,
+                          color: Colors.grey,
                         ),
+                        textAlign: TextAlign.center,
                       ),
-                    );
-                  },
-                  child: Card(
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(8),
-                      side: BorderSide(color: Colors.grey.shade300),
                     ),
-                    elevation: 2,
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Expanded(
-                          child: ClipRRect(
-                            borderRadius: const BorderRadius.only(
-                              topLeft: Radius.circular(8),
-                              topRight: Radius.circular(8),
-                            ),
-                            child: Image.asset(
-                              product['images'][0],
-                              width: double.infinity,
-                              fit: BoxFit.cover,
-                            ),
-                          ),
-                        ),
-                        Padding(
-                          padding: const EdgeInsets.all(8.0),
-                          child: Text(
-                            product['name']!,
-                            maxLines: 1,
-                            overflow: TextOverflow.ellipsis,
-                            style: const TextStyle(
-                              fontSize: 14.0,
-                              fontWeight: FontWeight.w500,
-                            ),
-                          ),
-                        ),
-                        Padding(
-                          padding: const EdgeInsets.symmetric(
-                              horizontal: 8.0, vertical: 5.0),
-                          child: Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                            children: [
-                              Text(
-                                product['price']!,
-                                style: const TextStyle(
-                                  fontSize: 16.0,
-                                  fontWeight: FontWeight.bold,
-                                  color: Colors.red,
+                  )
+                : Padding(
+                    padding: const EdgeInsets.only(top: 16.0),
+                    child: GridView.builder(
+                      physics: const NeverScrollableScrollPhysics(),
+                      shrinkWrap: true,
+                      gridDelegate:
+                          const SliverGridDelegateWithFixedCrossAxisCount(
+                        crossAxisCount: 2,
+                        crossAxisSpacing: 0,
+                        mainAxisSpacing: 0,
+                        childAspectRatio: 0.75,
+                      ),
+                      itemCount: _filteredProducts.length,
+                      itemBuilder: (context, index) {
+                        final product = _filteredProducts[index];
+                        return GestureDetector(
+                          onTap: () {
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                builder: (context) => DetailPage(
+                                  images: List<String>.from(product['images']),
+                                  name: product['name']!,
+                                  price: product['price']!,
+                                  sold: int.tryParse(
+                                          product['sold'].toString()) ??
+                                      0,
+                                  description: product['description'] ?? '',
                                 ),
                               ),
-                              Text(
-                                'Terjual: ${Helper.formatSold(product['sold']!)}',
-                                style: const TextStyle(
-                                  fontSize: 12.0,
-                                  color: Colors.grey,
+                            );
+                          },
+                          child: Card(
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(8),
+                              side: BorderSide(color: Colors.grey.shade300),
+                            ),
+                            elevation: 2,
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Expanded(
+                                  child: ClipRRect(
+                                    borderRadius: const BorderRadius.only(
+                                      topLeft: Radius.circular(8),
+                                      topRight: Radius.circular(8),
+                                    ),
+                                    child: Image.asset(
+                                      product['images'][0],
+                                      width: double.infinity,
+                                      fit: BoxFit.cover,
+                                    ),
+                                  ),
                                 ),
-                              ),
-                            ],
+                                Padding(
+                                  padding: const EdgeInsets.all(8.0),
+                                  child: Text(
+                                    product['name']!,
+                                    maxLines: 1,
+                                    overflow: TextOverflow.ellipsis,
+                                    style: const TextStyle(
+                                      fontSize: 14.0,
+                                      fontWeight: FontWeight.w500,
+                                    ),
+                                  ),
+                                ),
+                                Padding(
+                                  padding: const EdgeInsets.symmetric(
+                                      horizontal: 8.0, vertical: 5.0),
+                                  child: Row(
+                                    mainAxisAlignment:
+                                        MainAxisAlignment.spaceBetween,
+                                    children: [
+                                      Text(
+                                        product['price']!,
+                                        style: const TextStyle(
+                                          fontSize: 16.0,
+                                          fontWeight: FontWeight.bold,
+                                          color: Colors.red,
+                                        ),
+                                      ),
+                                      Text(
+                                        'Terjual: ${Helper.formatSold(product['sold']!)}',
+                                        style: const TextStyle(
+                                          fontSize: 12.0,
+                                          color: Colors.grey,
+                                        ),
+                                      ),
+                                    ],
+                                  ),
+                                ),
+                              ],
+                            ),
                           ),
-                        ),
-                      ],
+                        );
+                      },
                     ),
                   ),
-                );
-              },
-            ),
           ],
         ),
       ),
